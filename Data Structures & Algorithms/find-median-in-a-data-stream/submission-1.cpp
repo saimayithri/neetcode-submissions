afbf2f4fde6d1441a9a83ba98b09e19e1149bc5b
T Sai Mayithri <saimayithri@gmail.com>
2026-06-18 10:47:12 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,45 @@
+class MedianFinder {
+public:
+    priority_queue <int> small;
+    priority_queue <int,vector<int>,greater<int>> large;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+
+        small.push(num);
+        if(!small.empty()&&!large.empty())
+        {
+            if(small.top()>large.top())
+            {
+                large.push(small.top()); small.pop();
+            }
+        }
+
+        if(small.size()>large.size()+1)
+        {
+            large.push(small.top()); small.pop();
+        }
+        if(large.size()>small.size()+1)
+        {
+            small.push(large.top()); large.pop();
+        }
+        
+    }
+    
+    double findMedian() {
+
+        if(small.size()==large.size())
+        {
+            return (small.top()+large.top())/2.0;
+        }
+
+        if(small.size()>large.size())
+        {
+            return  small.top();
+        }
+        return large.top();
+        
+    }
+};
