2aa7cc901b8e68b782e3d9ef4cf0b5ea0f6c365b
T Sai Mayithri <saimayithri@gmail.com>
2026-07-02 12:29:11 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        
+        //[prev1,prev2,curr....] window
+        int prev1=nums[0]; int prev2=max(prev1,nums[1]);
+        for(int i=2;i<n-1;i++){
+            //dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+            int curr= max(nums[i]+prev1,prev2);
+            prev1=prev2;
+            prev2=curr;
+        }
+        int ans=prev2;
+
+        prev1=nums[1]; prev2=max(prev1,nums[2]);
+        for(int i=3;i<n;i++){
+            //dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+            int curr= max(nums[i]+prev1,prev2);
+            prev1=prev2;
+            prev2=curr;
+        }
+        ans=max(prev2,ans);
+        return ans;
+    }
+};
