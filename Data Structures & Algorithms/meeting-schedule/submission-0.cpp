adbca17bc46a7f0c8ed2c95b5356fbfba5798aa4
T Sai Mayithri <saimayithri@gmail.com>
2026-07-05 07:46:24 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        
+        
+        int n=intervals.size();
+        
+        if(n<=1){return true;}
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b) 
+        {
+            return a.start < b.start; 
+        });
+        for(int i=1;i<n;i++){
+            if(intervals[i-1].end>intervals[i].start){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
